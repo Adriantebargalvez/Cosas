1e9ac8791c67b22aa4cea018ae6eea6399c99708
Adriantebargalvez <124157818+Adriantebargalvez@users.noreply.github.com>
2024-01-12 00:15:32 +0100

    cambios

--- fernando/websocker/server.js
+++ fernando/websocker/server.js
@@ -1,25 +1,63 @@
 const express=require("express");
 const app=express();
 app.use("/",express.static("./www"));
-app.listen(3000, ()=>{console.log( "Servidor web en el puerto 3800")});
+app.listen(3000, ()=>{console.log( "Servidor web en el puerto 3000")});
 
 const {WebSocketServer} = require("ws");
 const server = new WebSocketServer({port:443});
 cuantosHayConectados=0;
-server.on("connection",(ws)=>{
+
+server.on("connection", (ws) => {
     cuantosHayConectados++;
-   console.log("Alguien se ha conectado: Ya somos "+cuantosHayConectados);
-  
-   ws.send("Hola")
-   
-   ws.on("close",()=>{
-    cuantosHayConectados--;
-    console.log("Alguien se ha desconectado: quedamos "+cuantosHayConectados)
-   }) 
+    console.log("Alguien se ha conectado: Ya somos " + cuantosHayConectados);
+    
+
+    
+
+    ws.on("close", () => {
+        cuantosHayConectados--;
+        console.log("Alguien se ha desconectado: quedamos " + cuantosHayConectados);
+        broadcast(`${ws.nombre} se ha desconectado.`);
+        broadcast("ListaUsuarios");
+
+
+        
+    });
+
+    ws.on("message", (mensaje) => {
+    if (ws.nombre === undefined) {
+        // Si el nombre del usuario no está definido, es un mensaje de nombre
+        ws.nombre = mensaje;
+        ws.send("Bienvenido al Chat, " + mensaje + "<br>");
+        broadcast("ListaUsuarios");
+    } else {
+        // Si el nombre del usuario está definido, es un mensaje del chat
+        console.log("Mensaje recibido:" + mensaje);
+        broadcast(`${ws.nombre}: ${mensaje}`);
+        
+    }
+    
 });
-setInterval(()=>{
-    const numero = Math.floor(Math.random()*100);
-    server.clients.forEach(cliente => {
-    cliente.send(numero.toString());
-    })
-    },1000);
+    
+    function broadcast(message) {
+        server.clients.forEach(cliente => {
+        if (message === "ListaUsuarios") {
+            const listaUsuarios = obtenerListaUsuarios();
+            cliente.send(JSON.stringify(listaUsuarios.toString()));
+        } else {
+            cliente.send(message);
+        }
+    });
+    }
+    function obtenerListaUsuarios() {
+        const listaUsuarios = [];
+        server.clients.forEach(cliente => {
+            listaUsuarios.push(cliente.nombre);
+        });
+        return listaUsuarios;
+    }
+   
+});
+
+
+   
