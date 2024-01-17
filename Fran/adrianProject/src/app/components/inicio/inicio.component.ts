b347237e660172ef49ce869af41e800462e764cb
Adriantebargalvez <adriantebar19@gmail.com>
2024-01-17 14:34:35 +0100

    cambio

---
+++ Fran/adrianProject/src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent {
+
+ observable = new Observable((subscriber)=>{
+   subscriber.next(1)
+     subscriber.next(2)
+   subscriber.next(3)
+ });
+ datos:any;
+ constructor() {
+   this.comenzarObservable();
+ }
+ comenzarObservable(){
+   this.observable.subscribe({
+     next: value => {
+       this.datos=value;
+       console.log(value)
+     },
+     error: err => {
+       console.log((err))
+     },
+   complete:()=>{
+       console.log('complete');
+   }
+
+   })
+ }
+
+}
