6d9c600be61189228631706170b7302363e47bd6
Adriantebargalvez <124157818+Adriantebargalvez@users.noreply.github.com>
2024-01-30 00:59:56 +0100

    springboot

--- fernando/trabajo/src/main/java/com/Papeleria/trabajo/Producto.java
+++ fernando/trabajo/src/main/java/com/Papeleria/trabajo/Producto.java
@@ -1,10 +1,14 @@
 package com.Papeleria.trabajo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Producto {
     private long id;
     private String nombre;
     private long unidades;
-
     private long precio;
 
 
--- fernando/trabajo/src/main/java/com/Papeleria/trabajo/controllers/ProductoController.java
+++ fernando/trabajo/src/main/java/com/Papeleria/trabajo/controllers/ProductoController.java
@@ -1,13 +1,44 @@
 package com.Papeleria.trabajo.controllers;
 
+import com.Papeleria.trabajo.Producto;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/Producto")
 public class ProductoController {
     @RequestMapping()
-    public String getProductos(){
+    public String getProductos(Model modelo) {
+        List<Producto> productos = new ArrayList<Producto>();
+        Producto p1 = new Producto(1,"lapiz",4,1);
+        Producto p2 = new Producto(2,"boli",1,2);
+        Producto p3 = new Producto(3,"folios",25,1);
+        Producto p4 = new Producto(4,"gomas",4,3);
+
+        productos.add(p1);
+        productos.add(p2);
+        productos.add(p3);
+        productos.add(p4);
+
+        modelo.addAttribute("listaProductos",productos);
+
         return "ListadoProductos";
     }
+
+    @GetMapping("/{id}")
+    public String detallesProducto(@PathVariable("id") long id){
+        return "DetalleProducto.html";
+    }
+    @PostMapping
+public String nuevoProducto(@RequestBody Producto nuevoProducto){
+        return"NuevoProducto.html";
+}
+@PutMapping("/{id}")
+    public String cambiarNombre(@PathVariable("id") long id,@RequestParam("nombre") String nuevonombre){
+        return "NombreCambiadoProducto.html";
+}
 }
